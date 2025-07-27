4d64a70f0ca0eeaa2a5c94dc1da51fd1fa29b9d4
ItsbaileyX <baileyleah542@gmail.com>
2025-07-27 18:32:22 +0100

    fix some shit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 let darkMode: boolean = false;
+let darkModeToggle: HTMLElement | null = document.getElementById('dark-mode-toggle');
 
 function toggleDarkMode() {
   darkMode = !darkMode;
   document.documentElement.classList.toggle('dark', darkMode);
 }
+
+darkModeToggle?.addEventListener('click', toggleDarkMode);
