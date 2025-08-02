96f91a11bce704e7a694586843d1936988b9bc9c
ItsbaileyX <baileyleah542@gmail.com>
2025-08-02 13:40:52 +0100

    fix config issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
 	base: '/',
 	plugins: [tailwindcss()],
+	build: {
+		rollupOptions: {
+			input: {
+				main: './index.html',
+				games: './games.html'
+			}
+		}
+	}
 });
